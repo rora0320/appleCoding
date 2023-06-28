4ae12673ae5e8eefb45a841a3476ee79ff65bf7a
kyeonghwa <lkhpgrc@gmail.com>
2023-06-28 22:42:02 +0900

    이벤트 버블링 응용 > 이벤트 리스너 하나로 탭기능 구현하기 parentElement로 부모요소 찾고 indexOf로 몇번째 자식구함 > 함수 자식 번호 넘기기

--- tab.js
+++ tab.js
@@ -1,9 +1,27 @@
-for(let i=0;i<$('.tab-button').length;i++) {
-    $('.tab-button').eq(i).on('click', function () {
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-        console.log('tab?');
-    })
+// for(let i=0;i<$('.tab-button').length;i++) {
+//     $('.tab-button').eq(i).on('click', function () {
+//         // $('.tab-button').removeClass('orange');
+//         // $('.tab-button').eq(i).addClass('orange');
+//         // $('.tab-content').removeClass('show');
+//         // $('.tab-content').eq(i).addClass('show');
+//         tabOpen(i);
+//         console.log('tab?');
+//     })
+// }
+// 1.eventListener only use one
+const dataset = {
+    0:'products',
+    1:'information',
+    2:'shopping'
+}
+$('.list').click(function(e){
+    const nodes = [...e.target.parentElement.children];
+    const targetIndex =nodes.indexOf(e.target);
+    tabOpen(targetIndex);
+})
+function tabOpen(index){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(index).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(index).addClass('show');
 }
